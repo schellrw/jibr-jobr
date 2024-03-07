c8bc60d6445523441499e562c81b7e9bc81b66aa
schellrw <schell.rw@gmail.com>
2024-03-06 22:27:17 -0600

    tweaks and less is more

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import streamlit as st
 
-headers = {"Authorization": f"Bearer {st.secrets.API_TOKEN}"}
+headers = [{"Authorization": f"Bearer {st.secrets.API_TOKEN}"}, 
+           {"Context":"Please summarize this into a job description."}]
 API_URL = f"https://api-inference.huggingface.co/models/{st.secrets.MODEL}"
 
 @st.cache_data
@@ -10,27 +11,26 @@ def query(payload):
     return response.json()
 
 # Add title and subtitle to the main interface of the app
-# st.title("Jibr-Jobr: turn the jibberish into a job description, and more!")
-st.title("Jibr-Jobr")
-st.subheader(":blue[Job Description Synthesis, at the Click of a Button.]")
-st.subheader(":orange[Instanty turn the jibberish into a job description that will attract ideal candidates!]")
+st.title("Jibr-Jobr: turn the jibberish into a job description, and more!")
+st.subheader(":blue[Job Description Synthesis, at the Click of a Button.\nTurn jibberish into a job description to attract top talent!]")
 
 #st.markdown("## Competitive intelligence and easy-to-use technology platforms are just a click away.")
 
 # Add sidebar to the app
-st.sidebar.markdown("### Contact us for Competitive Intelligence Solutions")
-st.sidebar.markdown("#### https://artificialintelligentsia.com/")
 st.sidebar.markdown(":blue[Developed at Artificial Intelligentsia, LLC]")
+st.sidebar.markdown("#### Contact us for Competitive Intelligence Solutions")
+st.sidebar.markdown("##### https://artificialintelligentsia.com/")
 st.sidebar.markdown(":gray[Copyright 2024. Artificial Intelligentsia, LLC.  All rights reserved.]")
 
 col1, col2 = st.columns(2)
 with col1:
-    text_input = st.text_area("Paste jibberish here...", height=112)
+    text_input = st.text_area("Hey Chuck, paste some job jibberish here...", height=420)
 with col2:
     submit = st.button("Sythesize")
-    if submit:
+    if text_input and submit:
         job_desc = query({"inputs": text_input})
-        st.write(job_desc)
+        text_output = st.text_area(st.write(job_desc), height=420)
+
         st.download_button(
             label="Download summary",
             data=job_desc,
